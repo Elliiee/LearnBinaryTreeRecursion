9c967ad891a4fb6694aba9d64dbd64509ae7e0e4
Elliiee <danyiluozhang@gmail.com>
2020-01-31 15:31:09 -0600

    Construct Binary Tree Inorder and Postorder

---
+++ BinaryTreeRecursion/src/ConstructBTInorderPostorder.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class ConstructBTInorderPostorder {
+    int post_idx;
+    int[] postorder;
+    int[] inorder;
+    HashMap<Integer, Integer> idx_map = new HashMap<>();
+
+    public TreeNode helper(int in_left, int in_right){
+        if (in_left > in_right)
+            return null;
+
+        int root_val = postorder[post_idx];
+        TreeNode root = new TreeNode(root_val);
+
+        int index = idx_map.get(root_val);
+
+        post_idx--;
+        root.right = helper(index + 1, in_right);
+        root.left = helper(in_left, index - 1);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder){
+        this.postorder = postorder;
+        this.inorder = inorder;
+
+        post_idx = postorder.length - 1;
+
+        int idx = 0;
+        for (Integer val : inorder)
+            idx_map.put(val, idx++);
+
+        return helper(0, inorder.length - 1);
+    }
+}
